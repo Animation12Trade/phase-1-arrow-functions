c147a952d7ca4f63b71ea81202f679907042aeac
+ <omuhindaaaron@gmail.com>
2024-03-21 08:40:08 +0300

    commit arrow functions

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = function(a,b) {return a/b};
+let a=2000;
+let b=100;
+
+const parameter = [2,5]
+// const square = parameter.map(x => x**2);
+function add(a, b) {
+    return a + b;
+}
+const square  = () => {
+    let x =2 
+    return x **2;
+}
